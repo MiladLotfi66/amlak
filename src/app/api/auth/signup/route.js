7b6460b573df222da0e8a420242ed9ed283a4bd2
miladlotfi <miladlotfi66@gmail.com>
2024-01-22 19:40:12 +0330

    connect signup form to database

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -6,26 +6,46 @@ import { hashPassword } from "@/utils/auth";
 export async function POST(req) {
   try {
     await connectDB();
+
     const { email, password } = await req.json();
-    console.log(email, password);
-    if (!email || !email.includes("@") || !password || password < 6) {
-      return new NextResponse("اطلاعات را به درستی وارد کنید", { status: 400 });
+    console.log({ email, password });
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر وارد کنید" },
+        { status: 422 }
+      );
     }
+
     const existingUser = await User.findOne({ email });
     console.log(existingUser);
+
     if (existingUser) {
-      return new NextResponse("این ایمیل قبلا ثبت شده است", { status: 400 });
+      return NextResponse.json(
+        { error: "این حساب کاربری وجود دارد" },
+        { status: 422 }
+      );
     }
+
     const hashedPassword = await hashPassword(password);
+
     const newUser = await User.create({
-      email,
+      email: email,
       password: hashedPassword,
     });
     console.log(newUser);
-    return NextResponse.json( "ثبت نام با موفقیت انجام شد" , newUser);
 
+    return NextResponse.json(
+      { message: "حساب کاربری ایجاد شد" },
+      { status: 201 }
+    );
   } catch (err) {
     console.log(err);
-    return new NextResponse("مشکلی در سرور رخ داده است", { status: 500 });
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      {
+        status: 500,
+      }
+    );
   }
 }
--- src/components/templates/SignupPage.js
+++ src/components/templates/SignupPage.js
@@ -2,47 +2,73 @@
 import styles from "@/templates/SignupPage.module.css";
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { Toaster, toast } from "react-hot-toast";
+import Loader from "@/module/Loader";
 
 function SignupPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [rePassword, setRePassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const signupHandler = (e) => {
+  const router = useRouter();
+
+  const signupHandler = async (e) => {
     e.preventDefault();
+
     if (password !== rePassword) {
-      toast.error("رمز و تکرار آن با هم برابر نیستند");
+      toast.error("رمز و تکرار آن برابر نیست");
+      return;
+    }
+    setLoading(true);
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    setLoading(false);
+    if (res.status === 201) {
+      router.push("/signin");
+    } else {
+      toast.error(data.error);
     }
   };
+
   return (
     <div className={styles.form}>
+      <h4>فرم ثبت نام</h4>
       <form>
-        <h4>فرم ثبت نام</h4>
-        <lable>ایمیل:</lable>
+        <label>ایمیل:</label>
         <input
           type="text"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <lable>رمز عبور:</lable>
+        />
+        <label>رمز عبور:</label>
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <lable>تکرار رمز عبور:</lable>
+        />
+        <label>تکرار رمز عبور:</label>
         <input
           type="password"
           value={rePassword}
           onChange={(e) => setRePassword(e.target.value)}
-        ></input>
-        <button type="submit" onClick={signupHandler}>
-          ثبت نام
-        </button>
+        />
+        {loading ? (
+          <Loader />
+        ) : (
+          <button type="submit" onClick={signupHandler}>
+            ثبت نام
+          </button>
+        )}
       </form>
       <p>
-        حساب کاربری دارید ؟<Link href="/signin">ورود</Link>
+        حساب کاربری دارید؟
+        <Link href="/signin">ورود</Link>
       </p>
       <Toaster />
     </div>
