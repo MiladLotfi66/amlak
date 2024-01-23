35d39c0dfadab6e077bb97cc49437d3c75a5ec22
miladlotfi <miladlotfi66@gmail.com>
2024-01-23 16:18:39 +0330

    autorizition at client side

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,8 +1,13 @@
+"use client";
 import { FiLogIn } from "react-icons/fi";
+import { FaUserAlt } from "react-icons/fa";
 import styles from "@/layout/Header.module.css";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 function Header() {
+  const { data } = useSession();
+  console.log(data);
   return (
     <header className={styles.header}>
       <div>
@@ -15,12 +20,21 @@ function Header() {
           </li>
         </ul>
       </div>
-      <div className={styles.login}>
-        <Link href="/signin">
-            <FiLogIn/>
+      {data ? (
+        <div className={styles.login}>
+          <Link href="/dashbord">
+            <FaUserAlt />
+            <span>{data.user.email}</span>
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.login}>
+          <Link href="/signin">
+            <FiLogIn />
             <span>ورود</span>
-        </Link>
-      </div>
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
