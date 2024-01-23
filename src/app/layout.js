76c85c60de2c4233269d18c9af64940313df8363
miladlotfi <miladlotfi66@gmail.com>
2024-01-23 11:53:01 +0330

    make provider for auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Layout from "@/layout/Layout";
 import "./globals.css";
 import { yekan } from "@/utils/fonts";
+import NextAuthProvider from "@/providers/NextAuthProvider";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,10 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={yekan.className}>
-        <Layout>
-        {children}
-        </Layout>
-        </body>
+        <NextAuthProvider>
+          <Layout>{children}</Layout>
+        </NextAuthProvider>
+      </body>
     </html>
   );
 }
